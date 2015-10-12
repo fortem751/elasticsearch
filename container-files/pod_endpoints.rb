9925ab520d58bc99766dbd6a2249e7c6fb1a2de2
Udo Urbantschitsch <udo.urbantschitsch@cloudwerkstatt.com>
2015-10-12 09:37:27 +0100

    Changed Master to svc.cluster.local

--- container-files/pod_endpoints.rb
+++ container-files/pod_endpoints.rb
@@ -9,7 +9,7 @@ namespace = ENV['OPENSHIFT_BUILD_NAMESPACE']
 token = File.new("/var/run/secrets/kubernetes.io/serviceaccount/token", "r").read
 #token="eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJsb2dnZXIiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlY3JldC5uYW1lIjoiZGVmYXVsdC10b2tlbi1hdzAyOCIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50Lm5hbWUiOiJkZWZhdWx0Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQudWlkIjoiMDFlOWQ3ODItNzAyMC0xMWU1LWJlZTctMDAxZGQ4YjcxYzc2Iiwic3ViIjoic3lzdGVtOnNlcnZpY2VhY2NvdW50OmxvZ2dlcjpkZWZhdWx0In0.IK2DfvbThCkroCGVmBaz5H8PmfjQzB9V_q4CpppyyEzc0PLR-UdqPRBnwkf_tO8CKb9aEz44pp1g_nkNVdPyK6VUCT7N-sQVlDyOQZoE4VBVNHcw5Dz3OqxTlysIbO50f9UtyNIWnbr32XuqYGdWXTbxzjaGzjlr3bmA1s6MiRrgn-qZeGuUoANVUyVScCqjaWK4iNvO-5HiXcpAdq6lag5OsMZ1RCQf2XqF_8WzU7-oCZnbo5mwStAy7WN5Alw5hB_cvdsyp8cTa5rvnyjhnWP2OGVP_I3nUI43MevyzzL-hTFO0wjNZfVSq1WV7qE3TareBt1bKTAjNYLt2TCl5g"
 
-url = URI.parse("https://mgmxasmastert01.infra.rit-paas.com:8443/api/v1/namespaces/#{namespace}/endpoints")
+url = URI.parse("https://openshift.default.svc.cluster.local/api/v1/namespaces/#{namespace}/endpoints")
 
 http =  Net::HTTP.new(url.host, url.port)
 http.use_ssl = true

2c74305d136fe088de334bb3fdcaff02e745a3a0
Udo Urbantschitsch <udourbantschitsch@Udos-MacBook-Air-2.local>
2015-11-14 21:28:45 +0100

    Changed to 2.0 - explicit IP

--- container-files/docker-entrypoint.sh
+++ container-files/docker-entrypoint.sh
@@ -5,10 +5,14 @@ if [ ! -z $OPENSHIFT_BUILD_NAMESPACE ] && [ -f /var/run/secrets/kubernetes.io/se
 
   eps=$(/pod_endpoints.rb)
 
+  IP=$(ip addr show eth0 | grep "inet " | awk '{split($0,a," "); print a[2]}')
+  IP=${IP:0:-3}
+
   elasticsearch -Des.node.master=$node_master \
       -Des.node.data=$node_data \
       -Des.http.enabled=$http_enabled \
-      -Des.discovery.zen.ping.unicast.hosts=$eps
+      -Des.discovery.zen.ping.unicast.hosts=$eps \
+      --network.host $IP
 else
   # Not on OpenShift -> Run in Multicast Mode
   echo "----------------------------------- Multicast Mode -----------------------------------"
